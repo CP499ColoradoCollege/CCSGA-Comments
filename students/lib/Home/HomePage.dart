032c7eb518379cf20bcf2bfc542260069fc67b88
SamDoggett <sdoggett@awarex.com>
2021-02-15 16:54:57 -0700

    Added our mission to home page

--- students/lib/Home/HomePage.dart
+++ students/lib/Home/HomePage.dart
@@ -17,8 +17,17 @@ class _HomePageState extends BaseState<HomePage> with BasicPage {
   @override
   Widget body() {
     return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10),
       child: Column(
         children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              'Announcements',
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          //Announcements Grid
           GridView.extent(
               maxCrossAxisExtent: 800.0,
               crossAxisSpacing: 5.0,
@@ -35,6 +44,45 @@ class _HomePageState extends BaseState<HomePage> with BasicPage {
                     'https://www.coloradocollege.edu/other/coronavirus/'),
                 HomeCard('CCSGA Announcement', 'Midnight rasties!!!')
               ]),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              'About Us',
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          //Our Mission Card
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.amberAccent),
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20)),
+                      color: Colors.amberAccent[700]),
+                  child: ListTile(
+                    leading: new IconTheme(
+                      data: new IconThemeData(color: Colors.white),
+                      child: new Icon(Icons.local_police_outlined),
+                    ),
+                    title: Text(
+                      "Our Mission",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                      "The Colorado College Student Government Association (CCSGA) is comprised of members democratically elected by the Colorado College student body. CCSGA gives students a crucial role in the campus-wide development of an enriching college experience. CCSGA strives to make life at Colorado College not only intellectual, but also enjoyable and meaningful. In addition to being a source of support for student organizations and events, CCSGA is also a forum for cooperative action and provides a voice calling for progress. CCSGA is dedicated to the improvement of Colorado College and is driven by the passion and determination of its students."),
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );

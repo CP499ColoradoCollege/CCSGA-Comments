5b0f950d303f47492d68dcfdaaeed7577a84add6
Ely Merenstein <elysamuel16@gmail.com>
2021-02-21 23:32:56 -0700

    Implement stored procedure and the necessary portion of the API PATCH route for revealing one's own identity in a conversation

--- backend/database_handler.py
+++ backend/database_handler.py
@@ -151,6 +151,20 @@ def create_stored_procedures():
                     SELECT -200;
                 END IF;
             END ;
+        ''',
+        '''CREATE PROCEDURE reveal_identity (IN conversationIdToUpdate INT, IN requester VARCHAR(40))
+            BEGIN
+                IF EXISTS (SELECT username FROM Users WHERE username=requester AND isBanned) THEN
+                    SELECT -403;
+                ELSEIF NOT EXISTS (SELECT id FROM Conversations WHERE id = conversationIdToUpdate) THEN
+                    SELECT -404;
+                ELSEIF NOT EXISTS (SELECT id FROM ConversationSettings WHERE username=requester AND conversationId=conversationIdToUpdate AND isAccessible) THEN
+                    SELECT -403;
+                ELSE
+                    UPDATE ConversationSettings SET identityRevealed = 1 WHERE username=requester AND id = conversationIdToUpdate;
+                    SELECT -200;
+                END IF;
+            END ;
         '''
     ]
 

e8bd55ef9d7d28c71385e17369f92f4c83f2ab7e
SamDoggett <sdoggett@awarex.com>
2021-02-22 09:50:11 -0700

    Updated admin fromJSON

--- students/lib/Models/Admins.dart
+++ students/lib/Models/Admins.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'User.dart';
 
 Admins adminsFromJson(String str) => Admins.fromJson(json.decode(str));
 
@@ -9,49 +10,17 @@ class Admins {
     this.admins,
   });
 
-  List<Admin> admins;
+  List<User> admins;
 
   factory Admins.fromJson(Map<String, dynamic> json) => Admins(
         admins: json["admins"] == null
             ? null
-            : List<Admin>.from(json["admins"].map((x) => Admin.fromJson(x))),
+            : List<User>.from(json["admins"].map((x) => User.fromJson(x))),
       );
 
   Map<String, dynamic> toJson() => {
         "admins": admins == null
             ? null
             : List<dynamic>.from(admins.map((x) => x.toJson())),
-      };
-}
-
-class Admin {
-  Admin({
-    this.displayName,
-    this.isAdmin,
-    this.isBanned,
-    this.isCcsga,
-    this.username,
-  });
-
-  String displayName;
-  bool isAdmin;
-  bool isBanned;
-  bool isCcsga;
-  String username;
-
-  factory Admin.fromJson(Map<String, dynamic> json) => Admin(
-        displayName: json["displayName"] == null ? null : json["displayName"],
-        isAdmin: json["isAdmin"] == null ? null : json["isAdmin"],
-        isBanned: json["isBanned"] == null ? null : json["isBanned"],
-        isCcsga: json["isCCSGA"] == null ? null : json["isCCSGA"],
-        username: json["username"] == null ? null : json["username"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "displayName": displayName == null ? null : displayName,
-        "isAdmin": isAdmin == null ? null : isAdmin,
-        "isBanned": isBanned == null ? null : isBanned,
-        "isCCSGA": isCcsga == null ? null : isCcsga,
-        "username": username == null ? null : username,
       };
 }

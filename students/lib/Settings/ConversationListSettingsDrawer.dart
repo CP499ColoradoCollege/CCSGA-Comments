5636048f0efdb867e0cb0c743c9784bcd5f4686e
SamDoggett <sdoggett@awarex.com>
2021-02-21 17:50:32 -0700

    Add futurebuilder for conversation filter

--- students/lib/Settings/ConversationListSettingsDrawer.dart
+++ students/lib/Settings/ConversationListSettingsDrawer.dart
@@ -1,6 +1,11 @@
 import 'package:ccsga_comments/ConversationList/ConversationListPage.dart';
+import 'package:ccsga_comments/Models/ChewedResponseModel.dart';
+import 'package:ccsga_comments/Models/User.dart';
 import 'package:flutter/material.dart';
 import 'package:ccsga_comments/Models/FilterEnums.dart';
+import 'package:tuple/tuple.dart';
+
+import '../DatabaseHandler.dart';
 
 class ConversationListSettingsDrawer extends StatefulWidget {
   Function(FilterByDate date, FilterByLabel label, bool isApartOfConversation)
@@ -16,6 +21,7 @@ class ConversationListSettingsDrawer extends StatefulWidget {
 
 class _ConversationListSettingsDrawerState
     extends State<ConversationListSettingsDrawer> {
+  User currentUser;
   FilterByDate sortByDateValue = FilterByDate.DateDescending;
   FilterByLabel sortByLabelValue = FilterByLabel.All;
   bool isApartOfConversation = true;
@@ -23,99 +29,130 @@ class _ConversationListSettingsDrawerState
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          Center(
-              child: Padding(
-            padding: EdgeInsets.all(10),
-            child: Text(
-              "Conversations Filter",
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-          )),
-          CheckboxListTile(
-            title: const Text('My Conversations'),
-            value: isApartOfConversation,
-            onChanged: (bool value) {
-              setState(() {
-                isApartOfConversation = value;
-                widget.filterCallback(
-                    sortByDateValue, sortByLabelValue, isApartOfConversation);
-              });
-            },
-            // secondary: const Icon(Icons.add_comment_outlined),
-          ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: DropdownButton<FilterByLabel>(
-              value: sortByLabelValue,
-              icon: Icon(Icons.arrow_downward),
-              iconSize: 24,
-              elevation: 16,
-              underline: Container(
-                height: 2,
-                color: Theme.of(context).accentColor,
-              ),
-              onChanged: (FilterByLabel newValue) {
-                setState(() {
-                  sortByLabelValue = newValue;
-                  widget.filterCallback(
-                      sortByDateValue, sortByLabelValue, isApartOfConversation);
-                });
-              },
-              items: [
-                DropdownMenuItem(
-                  child: Text("All Committees"),
-                  value: FilterByLabel.All,
-                ),
-                DropdownMenuItem(
-                  child: Text("Internal Affairs"),
-                  value: FilterByLabel.InternalAffairs,
-                ),
-                DropdownMenuItem(
-                  child: Text("Outreach"),
-                  value: FilterByLabel.Outreach,
-                ),
-                DropdownMenuItem(
-                  child: Text("Diversity and Inclusion"),
-                  value: FilterByLabel.DiversityAndInclusion,
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: DropdownButton<FilterByDate>(
-              value: sortByDateValue,
-              icon: Icon(Icons.arrow_downward),
-              iconSize: 24,
-              elevation: 16,
-              underline: Container(
-                height: 2,
-                color: Theme.of(context).accentColor,
-              ),
-              onChanged: (FilterByDate newValue) {
-                setState(() {
-                  sortByDateValue = newValue;
-                  widget.filterCallback(
-                      sortByDateValue, sortByLabelValue, isApartOfConversation);
-                });
-              },
-              items: [
-                DropdownMenuItem(
-                  child: Text("Date Descending"),
-                  value: FilterByDate.DateDescending,
-                ),
-                DropdownMenuItem(
-                  child: Text("Date Ascending"),
-                  value: FilterByDate.DateAscending,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+      child: FutureBuilder<bool>(
+          future: _getUserData(),
+          builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+            if (snapshot.hasData) {
+              return ListView(
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  Center(
+                      child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      "Conversations Filter",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                  )),
+                  currentUser.isCcsga
+                      ? CheckboxListTile(
+                          title: const Text('My Conversations'),
+                          value: isApartOfConversation,
+                          onChanged: (bool value) {
+                            setState(() {
+                              isApartOfConversation = value;
+                              widget.filterCallback(sortByDateValue,
+                                  sortByLabelValue, isApartOfConversation);
+                            });
+                          },
+                          // secondary: const Icon(Icons.add_comment_outlined),
+                        )
+                      : Container(),
+                  currentUser.isCcsga
+                      ? Padding(
+                          padding: EdgeInsets.all(10),
+                          child: DropdownButton<FilterByLabel>(
+                            value: sortByLabelValue,
+                            icon: Icon(Icons.arrow_downward),
+                            iconSize: 24,
+                            elevation: 16,
+                            underline: Container(
+                              height: 2,
+                              color: Theme.of(context).accentColor,
+                            ),
+                            onChanged: (FilterByLabel newValue) {
+                              setState(() {
+                                sortByLabelValue = newValue;
+                                widget.filterCallback(sortByDateValue,
+                                    sortByLabelValue, isApartOfConversation);
+                              });
+                            },
+                            items: [
+                              DropdownMenuItem(
+                                child: Text("All Committees"),
+                                value: FilterByLabel.All,
+                              ),
+                              DropdownMenuItem(
+                                child: Text("Internal Affairs"),
+                                value: FilterByLabel.InternalAffairs,
+                              ),
+                              DropdownMenuItem(
+                                child: Text("Outreach"),
+                                value: FilterByLabel.Outreach,
+                              ),
+                              DropdownMenuItem(
+                                child: Text("Diversity and Inclusion"),
+                                value: FilterByLabel.DiversityAndInclusion,
+                              ),
+                            ],
+                          ),
+                        )
+                      : Container(),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: DropdownButton<FilterByDate>(
+                      value: sortByDateValue,
+                      icon: Icon(Icons.arrow_downward),
+                      iconSize: 24,
+                      elevation: 16,
+                      underline: Container(
+                        height: 2,
+                        color: Theme.of(context).accentColor,
+                      ),
+                      onChanged: (FilterByDate newValue) {
+                        setState(() {
+                          sortByDateValue = newValue;
+                          widget.filterCallback(sortByDateValue,
+                              sortByLabelValue, isApartOfConversation);
+                        });
+                      },
+                      items: [
+                        DropdownMenuItem(
+                          child: Text("Date Descending"),
+                          value: FilterByDate.DateDescending,
+                        ),
+                        DropdownMenuItem(
+                          child: Text("Date Ascending"),
+                          value: FilterByDate.DateAscending,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            } else {
+              return SizedBox(
+                width: 50,
+                height: 50,
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
     );
+  }
+
+  Future<bool> _getUserData() async {
+    Tuple2<ChewedResponse, User> userResponse =
+        await DatabaseHandler.instance.getAuthenticatedUser();
+
+    if (userResponse.item2 != null) {
+      print("user response successful");
+      currentUser = userResponse.item2;
+      return true;
+    } else {
+      print("user response unsuccessful");
+      return false;
+    }
   }
 }

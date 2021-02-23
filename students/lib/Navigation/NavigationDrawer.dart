961c21576c671be3549a4195d4fa1803cd3334c2
SamDoggett <sdoggett@awarex.com>
2021-02-23 16:14:00 -0700

    Admin controls added to navigation (if admin)

--- students/lib/Navigation/NavigationDrawer.dart
+++ students/lib/Navigation/NavigationDrawer.dart
@@ -1,9 +1,14 @@
 import 'dart:html';
 
+import 'package:ccsga_comments/Models/ChewedResponseModel.dart';
+import 'package:ccsga_comments/Models/User.dart';
 import 'package:ccsga_comments/Navigation/CCSGABeamLocations.dart';
 import 'package:flutter/material.dart';
 import 'package:beamer/beamer.dart';
+import 'package:tuple/tuple.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+import '../DatabaseHandler.dart';
 
 class NavigationDrawer extends StatefulWidget {
   bool hasHeader = true;
@@ -18,6 +23,14 @@ class NavigationDrawer extends StatefulWidget {
 }
 
 class _NavigationDrawerState extends State<NavigationDrawer> {
+  Future<User> currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    currentUser = _getUserData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -76,6 +89,32 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
               context.beamTo(ConversationListLocation());
             },
           ),
+          FutureBuilder<User>(
+              future: currentUser,
+              builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+                if (snapshot.hasData) {
+                  return ListTile(
+                    title: Center(
+                      child: Text(
+                        'Admin Controls',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    contentPadding: EdgeInsets.all(10),
+                    onTap: () {
+                      print("Admin controls tapped");
+                      // Then close the drawer
+                      if (widget.hasHeader) {
+                        Navigator.pop(context);
+                      }
+
+                      context.beamTo(AdminLocation());
+                    },
+                  );
+                } else {
+                  return Container();
+                }
+              }),
           ListTile(
             title: Center(
               child: Text(
@@ -104,6 +143,19 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
     const url = "https://dev-cp499.coloradocollege.edu:8003/logout";
     if (await canLaunch(url)) {
       await launch(url);
+    }
+  }
+
+  Future<User> _getUserData() async {
+    Tuple2<ChewedResponse, User> userResponse =
+        await DatabaseHandler.instance.getAuthenticatedUser();
+
+    if (userResponse.item2 != null) {
+      print("user response successful");
+      return userResponse.item2;
+    } else {
+      print("user response unsuccessful");
+      return null;
     }
   }
 }
